0557ef3c2a34fac4063df9d37d6f771e8136a5df
apktntaj <maiaelarsya@gmail.com>
2024-11-23 11:31:40 +0700

    Simple quiz with C#

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] questions = {
+            "Siapa Tuhanmu?",
+            "Apa Agamamu?",
+            "Siapa Nabimu?",
+        };
+
+        string[] answers = {
+            "Allah",
+            "Islam",
+            "Muhammad",
+        };
+
+        int score = 0;
+
+        Console.WriteLine("");
+        int i = 0;
+        do
+        {
+            Console.WriteLine(questions[i]);
+            string answer = Console.ReadLine();
+            if (answer.ToLower() == answers[i].ToLower())
+            {
+                Console.WriteLine("Benar!");
+                score+=10;
+            }
+            else
+            {
+                Console.WriteLine("Salah!");
+            }
+        } while (i < questions.Length);
+        Console.WriteLine("Skor akhir: " + score);
+    }
+}
